028dbc873a30ef3af2d878804b789766ec4fb6dd
Pavel Solovev <daron439@gmail.com>
2023-07-19 10:43:49 +0300

    disable fedora-repos-archive

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 
 set -ouex pipefail
 
-sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{modular,updates-modular}.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-{updates-archive,modular,updates-modular}.repo
 
 rpm-ostree install \
     https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm \
-    /tmp/rpms/*.rpm \
-    fedora-repos-archive
+    /tmp/rpms/*.rpm #\
+    # fedora-repos-archive
 
 install -D /tmp/ublue-os-nvidia-addons/rpmbuild/SOURCES/nvidia-container-runtime.repo \
     /etc/yum.repos.d/nvidia-container-runtime.repo
